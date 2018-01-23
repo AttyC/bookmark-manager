1d57451cf118708332475eaddbc63279b12a65f4
Tagrand <thomas.a.grand@gmail.com>
2018-01-23 14:22:26 +0000

    add new bookmark

--- app/app.rb
+++ app/app.rb
@@ -3,10 +3,21 @@ require_relative 'models/link'
 
 class Bookmarks < Sinatra::Base
 
+  enable :sessions
+
   get '/links' do
     @links = Link.all
     p @links
     erb :link
+  end
+
+  get '/links/new' do
+    erb :new
+  end
+
+  post '/save' do
+    Link.create(title:  params["title"], url: params["url"])
+    redirect '/links/new'
   end
 
   run! if app_file == $0
