8d6bf9c7b142bc015eadc04507814f0137dc8b3b
Tagrand <thomas.a.grand@gmail.com>
2018-01-24 14:52:03 +0000

    change tag name

--- spec/feature/add_tag_spec.rb
+++ spec/feature/add_tag_spec.rb
@@ -4,7 +4,7 @@ feature 'add tag to link' do
     visit "/links/new"
     fill_in "title", with: "bing"
     fill_in "url", with: "www.bing.com"
-    fill_in "tag", with: "new tag"
+    fill_in "tag_name", with: "new tag"
     click_button "Save bookmark"
     click_button "Visit bookmarks"
     expect(page).to have_content "new tag"
